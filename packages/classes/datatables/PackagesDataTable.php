8708247061de51bb876f64d2f7911cd0bc6b973e
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2020-02-08 02:15:13 -0400

    Copyright updated

--- packages/classes/datatables/PackagesDataTable.php
+++ packages/classes/datatables/PackagesDataTable.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('ABS_PATH')) exit('ABS_PATH is not loaded. Direct access is not allowed.');
 
 /*
- * Copyright 2019 CodexiLab
+ * Copyright 2019 - 2020 CodexiLab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- packages/views/admin/settings.php
+++ packages/views/admin/settings.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- * Copyright 2019 CodexiLab
+ * Copyright 2019 - 2020 CodexiLab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
