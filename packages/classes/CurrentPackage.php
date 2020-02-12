58703752ac75022870fd0ac41dbaa8c47c1f8f0a
CodexiLab <52092651+codexilab@users.noreply.github.com>
2020-02-12 15:41:07 -0400

    Tabs for 4

--- packages/classes/CurrentPackage.php
+++ packages/classes/CurrentPackage.php
@@ -31,31 +31,31 @@ class CurrentPackage
 	{
 		$freeItems = 0;
 		$packageAssigned	= Packages::newInstance()->getAssigned(osc_logged_user_id());
-		$packageId		= (isset($packageAssigned['fk_i_package_id'])) ? $packageAssigned['fk_i_package_id'] : 0;
-		$assignmentId		= (isset($packageAssigned['pk_i_id'])) ? $packageAssigned['pk_i_id'] : 0;
+		$packageId 			= (isset($packageAssigned['fk_i_package_id'])) ? $packageAssigned['fk_i_package_id'] : 0;
+		$assignmentId 		= (isset($packageAssigned['pk_i_id'])) ? $packageAssigned['pk_i_id'] : 0;
 
 		if ($packageAssigned) {
 
-			$packageName		= get_package_name($packageId);
-			$fromDate		= $packageAssigned['dt_from_date'];
-			$untilDate		= $packageAssigned['dt_to_date'];
+			$packageName 	= get_package_name($packageId);
+			$fromDate 		= $packageAssigned['dt_from_date'];
+			$untilDate 		= $packageAssigned['dt_to_date'];
 
-			$packageItems		= get_package_free_items($packageId);
-			$publishedItems		= Packages::newInstance()->getTotalItemsByAssignment($assignmentId);
-			$freeItems		= $packageItems-$publishedItems; if ($freeItems < 0) $freeItems = 0;
+			$packageItems 	= get_package_free_items($packageId);
+			$publishedItems	= Packages::newInstance()->getTotalItemsByAssignment($assignmentId);
+			$freeItems 		= $packageItems-$publishedItems; if ($freeItems < 0) $freeItems = 0;
 
-			$this->currentPackage['id']			= (int) $packageId;
-			$this->currentPackage['assignment_id']		= (int) $assignmentId;
-			$this->currentPackage['name']			= (string) $packageName;
-			$this->currentPackage['free_items']		= (int) $freeItems; 		// Descending count, example: 3/5 (from five to three)
-			$this->currentPackage['published_items']	= (int) $publishedItems; 	// Ascending count, example: 2/5 (from two to five)
-			$this->currentPackage['package_items']		= (int) $packageItems;
-			$this->currentPackage['status']			= "assigned";
-			$this->currentPackage['from_date']		= $fromDate;
-			$this->currentPackage['until_date']		= $untilDate;
+			$this->currentPackage['id'] 				= (int) $packageId;
+			$this->currentPackage['assignment_id'] 		= (int) $assignmentId;
+			$this->currentPackage['name'] 				= (string) $packageName;
+			$this->currentPackage['free_items'] 		= (int) $freeItems; 		// Descending count, example: 3/5 (from five to three)
+			$this->currentPackage['published_items'] 	= (int) $publishedItems; 	// Ascending count, example: 2/5 (from two to five)
+			$this->currentPackage['package_items'] 		= (int) $packageItems;
+			$this->currentPackage['status'] 			= "assigned";
+			$this->currentPackage['from_date'] 			= $fromDate;
+			$this->currentPackage['until_date'] 		= $untilDate;
 
 			// true: is defeated | false: is not defeated
-			$this->currentPackage['defeated']		= check_date_interval($fromDate, $untilDate);
+			$this->currentPackage['defeated'] 			= check_date_interval($fromDate, $untilDate);
 		}
 
 		if ($packageAssigned && $freeItems == 0) {
